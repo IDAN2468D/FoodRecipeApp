f0d80b5561de1d4a518e007f9753f80f3adb679b
IDAN <48420836+IDAN2468D@users.noreply.github.com>
2021-06-11 11:16:16 +0300

    Update

---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    StatusBar
+} from 'react-native';
+
+import { image, COLORS, SIZES } from '../constants';
+
+const Home = ({ navigation }) => {
+    return (
+        <View
+
+        >
+
+        </View>
+    )
+}
+
+export default Home;
