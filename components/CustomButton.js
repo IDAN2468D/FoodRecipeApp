f45d17329546fddddacf4ebdc3673f37f6d74c4e
IDAN2468D <48420836+IDAN2468D@users.noreply.github.com>
2021-06-13 18:06:19 +0300

    Update

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -8,9 +8,7 @@ const CustomButton = ({ buttonText, buttonContainerStyle, colors, onPress }) => 
     if (colors.length > 0) {
         return (
             <TouchableOpacity onPress={onPress}>
-                <LinearGradient colors style={{
-                    ...buttonContainerStyle
-                }}>
+                <LinearGradient colors={colors} style={{...buttonContainerStyle}}>
                     <Text style={{ textAlign: "center", color: COLORS.white, ...FONTS.h2 }}>
                         {buttonText}
                     </Text>
